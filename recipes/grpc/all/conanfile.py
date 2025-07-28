58307786c18f8d1d0ca1ba74a8a524b37da16a78
Bart Thomee <11445373+bthomee@users.noreply.github.com>
2025-07-28 15:27:50 -0400

    Set grpc_MSVC_STATIC_RUNTIME option in grpc

--- recipes/grpc/all/conanfile.py
+++ recipes/grpc/all/conanfile.py
@@ -201,7 +201,6 @@ class GrpcConan(ConanFile):
         # (supported in gRPC >= 1.62.0)
         tc.cache_variables["gRPC_DOWNLOAD_ARCHIVES"] = False
 
-
         # Consumed targets (abseil) via interface target_compiler_feature can propagate newer standards
         if not valid_min_cppstd(self, self._cxxstd_required):
             tc.cache_variables["CMAKE_CXX_STANDARD"] = self._cxxstd_required
@@ -209,6 +208,11 @@ class GrpcConan(ConanFile):
         if is_apple_os(self):
             # workaround for: install TARGETS given no BUNDLE DESTINATION for MACOSX_BUNDLE executable
             tc.cache_variables["CMAKE_MACOSX_BUNDLE"] = False
+
+        if is_msvc(self) or self._is_clang_cl:
+            runtime = self.settings.get_safe("compiler.runtime")
+            if runtime:
+                tc.cache_variables["grpc_MSVC_STATIC_RUNTIME"] = runtime == "static"
 
         if self._supports_libsystemd:
             tc.cache_variables["gRPC_USE_SYSTEMD"] = self.options.with_libsystemd
