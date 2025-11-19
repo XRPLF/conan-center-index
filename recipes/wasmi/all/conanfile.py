bfcfb81564de94810bd492efde8d1821c7896ead
Oleksandr <115580134+oleks-rip@users.noreply.github.com>
2025-11-19 18:03:50 -0500

    Add wasmi recipe

---
+++ recipes/wasmi/all/conanfile.py
@@ -0,0 +1,57 @@
+from conan import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get
+import os
+
+required_conan_version = ">=2.0.0"
+
+class WasmiConan(ConanFile):
+    name = "wasmi"
+    license = "Apache License v2.0"
+    url = "https://github.com/wasmi-labs/wasmi.git"
+    description = "WebAssembly (Wasm) interpreter"
+    package_type = "library"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def config_options(self):
+        pass
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+
+        tc.variables["CMAKE_CXX_STANDARD"] = 20
+        tc.variables["BUILD_SHARED_LIBS"] = 0
+
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        apply_conandata_patches(self)
+        cmake = CMake(self)
+        cmake.verbose = True
+        cmake.configure(build_script_folder=os.path.join(self.source_folder, "crates", "c_api"))
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.verbose = True
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["wasmi"]
+        self.cpp_info.names["cmake_find_package"] = "wasmi"
+        self.cpp_info.names["cmake_find_package_multi"] = "wasmi"
+
