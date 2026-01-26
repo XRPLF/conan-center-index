8acc0f622c1b3cd64b4263590c300a7932323e6c
Bart <bthomee@users.noreply.github.com>
2026-01-26 13:38:53 +0000

    Bump Boost version in NuDB recipe

--- recipes/nudb/all/conanfile.py
+++ recipes/nudb/all/conanfile.py
@@ -28,7 +28,7 @@ class NudbConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("boost/1.89.0")
+        self.requires("boost/1.90.0")
 
     def package_id(self):
         self.info.clear()
