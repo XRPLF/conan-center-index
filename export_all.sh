377a75412300da8b8f4d379e7c114073f6b4a358
Ayaz Salikhov <asalikhov@ripple.com>
2026-03-23 23:33:48 +0000

    Simplify export workflow and add export_all script

---
+++ export_all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+DIRECTORY=$(dirname "$0")
+
+pushd "${DIRECTORY}"/recipes/
+
+conan export abseil/all --version=20250127.0
+conan export ed25519/all --version=2015.03
+conan export grpc/all --version=1.72.0
+conan export m4/all --version=1.4.19
+conan export mpt-crypto/all --version=0.2.0-rc1
+conan export nudb/all --version=2.0.9
+conan export openssl/3.x.x --version=3.5.4
+conan export secp256k1/all --version=0.7.1
+conan export snappy/all --version=1.1.10
+conan export soci/all --version=4.0.3
+conan export wasm-xrplf/all --version=2.4.1-xrplf
+conan export wasmi/all --version=1.0.6
+conan export wasmi/all --version=1.0.9
+
+popd
