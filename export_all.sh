324bb63661b9360f65baecc1cf3d3407373c97ce
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2026-03-25 17:04:11 +0000

    Export custom openssl 3.6.1 (#50)
    Let's try to update to openssl 3.6.1, I will check it works in rippled
    after merge

--- export_all.sh
+++ export_all.sh
@@ -13,6 +13,7 @@ conan export m4/all --version=1.4.19
 conan export mpt-crypto/all --version=0.2.0-rc1
 conan export nudb/all --version=2.0.9
 conan export openssl/3.x.x --version=3.5.5
+conan export openssl/3.x.x --version=3.6.1
 conan export secp256k1/all --version=0.7.1
 conan export snappy/all --version=1.1.10
 conan export soci/all --version=4.0.3
