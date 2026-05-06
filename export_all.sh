e7779c7d59ca79475df33cc7a4d377996ac4a369
Ayaz Salikhov <asalikhov@ripple.com>
2026-05-06 13:44:14 +0100

    Better yaml parsing

--- export_all.sh
+++ export_all.sh
@@ -7,11 +7,7 @@ function export_recipe() {
     local version="$2"
 
     # Check the version is available in conandata.yml
-    if ! grep -q "${version}" "${directory}/conandata.yml"; then
-        echo "Version ${version} not found in conandata.yml for ${directory}"
-        exit 1
-    fi
-
+    yq --exit-status ".sources | has(\"${version}\")" "${directory}/conandata.yml"
     conan export "${directory}" --version="${version}"
 }
 
@@ -19,10 +15,10 @@ DIRECTORY=$(dirname "$0")
 
 pushd "${DIRECTORY}"/recipes/
 
-export_recipe abseil/all 20250127
+export_recipe abseil/all 20250127.0
 export_recipe ed25519/all 2015.03
 export_recipe mpt-crypto/all 0.3.0-rc1
-export_recipe openssl/3.x.x 3.5.5
+export_recipe openssl/3.x.x 3.5.6
 export_recipe openssl/3.x.x 3.6.2
 export_recipe secp256k1/all 0.7.1
 export_recipe snappy/all 1.1.10
