8d95ecf8c13638a5b549d87a2be9ad3659c5f8ad
Alex Kremer <godexsoft@gmail.com>
2026-08-11 16:45:42 +0100

    chore: Publish `xrpl-rpc-spec` version 0.1.3 (#83)

---
+++ recipes/xrpl-rpc-spec/all/conanfile.py
@@ -0,0 +1,61 @@
+import os
+
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+
+required_conan_version = ">=2.0.0"
+
+
+class XrplRpcSpecConan(ConanFile):
+    name = "xrpl-rpc-spec"
+    description = "Consteval RPC spec DSL for XRPL: shared by Clio and xrpld"
+    license = "ISC"
+    url = "https://github.com/XRPLF/rpc-spec"
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
+
+    requires = [
+        "boost/1.91.0",
+    ]
+
+    default_options = {
+        "boost/*:without_cobalt": True,
+    }
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, 23)
+
+    def package(self):
+        copy(
+            self,
+            "*.hpp",
+            src=os.path.join(self.source_folder, "include"),
+            dst=os.path.join(self.package_folder, "include"),
+        )
+        copy(
+            self,
+            "LICENSE.md",
+            src=self.source_folder,
+            dst=os.path.join(self.package_folder, "licenses"),
+        )
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.includedirs = ["include"]
+        self.cpp_info.set_property("cmake_file_name", "xrpl-rpc-spec")
+        self.cpp_info.set_property("cmake_target_name", "rpcspec::rpcspec")
+        self.cpp_info.requires = ["boost::json"]
